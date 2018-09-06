616863e61dfd0688ec3369dae3aa96c1b3c4f9e0
Altin Selimi <altinselimi@msn.com>
2018-09-06 14:41:17 +0200

    fix demo options button

--- src/components/demo/options.vue
+++ src/components/demo/options.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="preferences-wrapper" :class="{'active': show_options}">
-		<button class="show_options">
+		<div class="show_options">
 			PREFERENCES
 			<span class="pref-icon" @click="show_options = !show_options">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders">
@@ -21,7 +21,7 @@
 					<path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
 				</svg>
 			</a>
-		</button>
+		</div>
 		<div class="pref-slot">
 			<slot></slot>
 		</div>
